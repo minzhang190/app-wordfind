fca57f08c2b219724d5441d9d5b22ba694c5ff58
minzhang190 <minzhang190@gmail.com>
2020-04-14 12:37:15 +0000

    Ensure sound files are loaded

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 document.title = title;
 
-Object.keys(words).forEach(function(word) {
+var wordText = Object.keys(words);
+
+var soundCount = 0;
+
+function handleLoad() {
+    if (++soundCount < wordText.length) {
+        return;
+    }
+
+    try {
+        var game = new WordFindGame('#puzzle', options);
+
+        wordfind.print(game);
+
+        $('#solve').click(() => game.solve());
+    } catch (error) {
+        location.reload();
+    }
+}
+
+createjs.Sound.addEventListener("fileload", handleLoad);
+
+wordText.forEach(function(word) {
     var wordInfo = words[word];
-    var $li = $('<li><img class="word"></li>');
+    var $li = wordInfo['element'] = $('<li><img class="word"></li>');
     $li.find('img').attr('src', prefix + wordInfo.image).data('info', wordInfo);
-    wordInfo['element'] = $li;
     $('#words').append($li);
 
     createjs.Sound.registerSound(prefix + wordInfo.sound, word);
 });
-
-try {
-    var game = new WordFindGame('#puzzle', options);
-
-    wordfind.print(game);
-
-    $('#solve').click(() => game.solve());
-} catch (error) {
-    location.reload();
-}

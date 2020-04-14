7fff2ec33b281427a711a76dfb6f63333fce03c2
minzhang190 <minzhang190@gmail.com>
2020-04-14 12:37:46 +0000

    Shuffle

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 document.title = title;
 
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
 var wordText = Object.keys(words);
+shuffleArray(wordText);
 
 var soundCount = 0;
 

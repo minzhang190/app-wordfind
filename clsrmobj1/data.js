eebc15eacd8fa7f758ebe108416956493bf1908e
minzhang190 <minzhang190@gmail.com>
2020-04-21 15:03:38 +0000

    clsrmobj1: remove jiaoshiyongpin/shu/qianbihe

--- clsrmobj1/data.js
+++ clsrmobj1/data.js
@@ -5,18 +5,18 @@ var words = {
         "image": "assets/5e9cc6ec83dd7905aa16c67f.png",
         "sound": "assets/5e9cc6ec83dd7905aa16c67f.mp3"
     },
-    "jiaoshiyongpin": {
-        "image": "assets/5e9cc6ec83dd7905aa16c680.png",
-        "sound": "assets/5e9cc6ec83dd7905aa16c680.mp3"
-    },
+    //"jiaoshiyongpin": {
+    //    "image": "assets/5e9cc6ec83dd7905aa16c680.png",
+    //    "sound": "assets/5e9cc6ec83dd7905aa16c680.mp3"
+    //},
     "shubao": {
         "image": "assets/5e8dafd4fe028112aa073e72.png",
         "sound": "assets/5e8dafd4fe028112aa073e72.mp3"
     },
-    "shu": {
-        "image": "assets/5e8dafd4fe028112aa073e6e.png",
-        "sound": "assets/5e8dafd4fe028112aa073e6e.mp3"
-    },
+    //"shu": {
+    //    "image": "assets/5e8dafd4fe028112aa073e6e.png",
+    //    "sound": "assets/5e8dafd4fe028112aa073e6e.mp3"
+    //},
     "benzi": {
         "image": "assets/5e9cc6ee83dd7905aa16c681.png",
         "sound": "assets/5e9cc6ee83dd7905aa16c681.mp3"
@@ -29,10 +29,10 @@ var words = {
         "image": "assets/5e9cc6ee83dd7905aa16c683.png",
         "sound": "assets/5e9cc6ee83dd7905aa16c683.mp3"
     },
-    "qianbihe": {
-        "image": "assets/5e9cc6ef83dd7905aa16c684.png",
-        "sound": "assets/5e9cc6ef83dd7905aa16c684.mp3"
-    },
+    //"qianbihe": {
+    //    "image": "assets/5e9cc6ef83dd7905aa16c684.png",
+    //    "sound": "assets/5e9cc6ef83dd7905aa16c684.mp3"
+    //},
     "caibi": {
         "image": "assets/5e9cc6ef83dd7905aa16c685.png",
         "sound": "assets/5e9cc6ef83dd7905aa16c685.mp3"

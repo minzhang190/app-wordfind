18cf5316796ef73abc4bf3c37383d46f8e9e1812
minzhang190 <minzhang190@gmail.com>
2020-05-18 05:28:23 +0000

    +animals1/2

---
+++ animals1/data.js
@@ -0,0 +1,38 @@
+var title = "Dòngwu (1)";
+
+var words = {
+    "dongwu": {
+        "image": "assets/5e9cda08037c6b87b73250f1.png",
+        "sound": "assets/5e9cda08037c6b87b73250f1.mp3"
+    },
+    "niu": {
+        "image": "assets/5e9cda09037c6b87b73250f2.png",
+        "sound": "assets/5e9cda09037c6b87b73250f2.mp3"
+    },
+    "shanyang": {
+        "image": "assets/5e9cda09037c6b87b73250f3.png",
+        "sound": "assets/5e9cda09037c6b87b73250f3.mp3"
+    },
+    "mianyang": {
+        "image": "assets/5e9cda09037c6b87b73250f4.png",
+        "sound": "assets/5e9cda09037c6b87b73250f4.mp3"
+    },
+    "gou": {
+        "image": "assets/5e8dab8369c2516911ea4531.png",
+        "sound": "assets/5e8dab8369c2516911ea4531.mp3"
+    },
+    "mao": {
+        "image": "assets/5e8dab8369c2516911ea4530.png",
+        "sound": "assets/5e8dab8369c2516911ea4530.mp3"
+    },
+    "gongji": {
+        "image": "assets/5e9cda0b037c6b87b73250f5.png",
+        "sound": "assets/5e9cda0b037c6b87b73250f5.mp3"
+    },
+    "muji": {
+        "image": "assets/5e9cda0b037c6b87b73250f6.png",
+        "sound": "assets/5e9cda0b037c6b87b73250f6.mp3"
+    }
+};
+
+options.orientations = ['horizontal', 'vertical', 'diagonal', 'diagonalBack'];
---
+++ animals2/data.js
@@ -0,0 +1,36 @@
+var title = "Dòngwu (2)";
+
+var words = {
+    "xiaoji": {
+        "image": "assets/5e9cda0b037c6b87b73250f7.png",
+        "sound": "assets/5e9cda0b037c6b87b73250f7.mp3"
+    },
+    "huoji": {
+        "image": "assets/5e9cda0c037c6b87b73250f8.png",
+        "sound": "assets/5e9cda0c037c6b87b73250f8.mp3"
+    },
+    "yazi": {
+        "image": "assets/5e8dab8369c2516911ea4532.png",
+        "sound": "assets/5e8dab8369c2516911ea4532.mp3"
+    },
+    "e": {
+        "image": "assets/5e9cda0d037c6b87b73250f9.png",
+        "sound": "assets/5e9cda0d037c6b87b73250f9.mp3"
+    },
+    "ma": {
+        "image": "assets/5e9cda0d037c6b87b73250fa.png",
+        "sound": "assets/5e9cda0d037c6b87b73250fa.mp3"
+    },
+    "zhu": {
+        "image": "assets/5e9cda0d037c6b87b73250fb.png",
+        "sound": "assets/5e9cda0d037c6b87b73250fb.mp3"
+    },
+    "tuzi": {
+        "image": "assets/5e9cda0e037c6b87b73250fc.png",
+        "sound": "assets/5e9cda0e037c6b87b73250fc.mp3"
+    },
+    "lü": {
+        "image": "assets/5e9cda0e037c6b87b73250fd.png",
+        "sound": "assets/5e9cda0e037c6b87b73250fd.mp3"
+    }
+};

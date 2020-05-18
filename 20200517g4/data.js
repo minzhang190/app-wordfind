c5e89de1b519404b6d64394cf8cff77f62eddd51
minzhang190 <minzhang190@gmail.com>
2020-05-18 04:06:44 +0000

    +20200517g4

---
+++ 20200517g4/data.js
@@ -0,0 +1,48 @@
+var title = "Jīntiān tiānqì zěnme yàng?";
+
+var words = {
+    "zenmeyang": {
+        "image": "assets/5e9cd75dfb76c53278b7ea5c.png",
+        "sound": "assets/5e9cd75dfb76c53278b7ea5c.mp3"
+    },
+    "nage": {
+        "image": "assets/5e9cd75dfb76c53278b7ea5d.png",
+        "sound": "assets/5e9cd75dfb76c53278b7ea5d.mp3"
+    },
+    "shenme": {
+        "image": "assets/5e9cd75efb76c53278b7ea5e.png",
+        "sound": "assets/5e9cd75efb76c53278b7ea5e.mp3"
+    },
+    "shi": {
+        "image": "assets/5e9cd75ffb76c53278b7ea5f.png",
+        "sound": "assets/5e9cd75ffb76c53278b7ea5f.mp3"
+    },
+    "henhao": {
+        "image": "assets/5e9cd75ffb76c53278b7ea60.png",
+        "sound": "assets/5e9cd75ffb76c53278b7ea60.mp3"
+    },
+    "buhao": {
+        "image": "assets/5e9cd760fb76c53278b7ea61.png",
+        "sound": "assets/5e9cd760fb76c53278b7ea61.mp3"
+    },
+    "jintian": {
+        "image": "assets/5e9cd58117c76e7d6c69f905.png",
+        "sound": "assets/5e9cd58117c76e7d6c69f905.mp3"
+    },
+    "zuotian": {
+        "image": "assets/5e9cd58217c76e7d6c69f907.png",
+        "sound": "assets/5e9cd58217c76e7d6c69f907.mp3"
+    },
+    "mingtian": {
+        "image": "assets/5e9cd58117c76e7d6c69f906.png",
+        "sound": "assets/5e9cd58117c76e7d6c69f906.mp3"
+    },
+    "qiantian": {
+        "image": "assets/5ea3b71497e2a3b784762d36.png",
+        "sound": "assets/5ea3b71497e2a3b784762d36.mp3"
+    },
+    "houtian": {
+        "image": "assets/5ea3b71497e2a3b784762d37.png",
+        "sound": "assets/5ea3b71497e2a3b784762d37.mp3"
+    }
+};

cbf32edb9b6b37fb1a5713693a09170470377d98
minzhang190 <minzhang190@gmail.com>
2020-09-08 05:59:06 +0000

    +colorsprod2

---
+++ colorsprod2/data.js
@@ -0,0 +1,46 @@
+var title = "Yánsè";
+
+var words = {
+    "hongse": {
+        "image": "assets/5e8dace2ebcacede29732460.png",
+        "sound": "assets/5e8dace2ebcacede29732460.mp3"
+    },
+    "chengse": {
+        "image": "assets/5e8dace2ebcacede2973245c.png",
+        "sound": "assets/5e8dace2ebcacede2973245c.mp3"
+    },
+    "huangse": {
+        "image": "assets/5e8dace2ebcacede29732462.png",
+        "sound": "assets/5e8dace2ebcacede29732462.mp3"
+    },
+    "lüse": {
+        "image": "assets/5e8dace2ebcacede2973245a.png",
+        "sound": "assets/5e8dace2ebcacede2973245a.mp3"
+    },
+    "lanse": {
+        "image": "assets/5e8dace2ebcacede29732458.png",
+        "sound": "assets/5e8dace2ebcacede29732458.mp3"
+    },
+    "zise": {
+        "image": "assets/5e8dace2ebcacede2973245e.png",
+        "sound": "assets/5e8dace2ebcacede2973245e.mp3"
+    },
+    "heise": {
+        "image": "assets/5e8dace2ebcacede29732457.png",
+        "sound": "assets/5e8dace2ebcacede29732457.mp3"
+    },
+    "baise": {
+        "image": "assets/5e8dace2ebcacede29732461.png",
+        "sound": "assets/5e8dace2ebcacede29732461.mp3"
+    },
+    "kafeise": {
+        "image": "assets/5e8dace2ebcacede29732459.png",
+        "sound": "assets/5e8dace2ebcacede29732459.mp3"
+    },
+    "fense": {
+        "image": "assets/5e8dace2ebcacede2973245d.png",
+        "sound": "assets/5e8dace2ebcacede2973245d.mp3"
+    }
+};
+
+options.orientations = ['horizontal', 'vertical'];

282b9e266f9bdc6880079dc44535522dcf35fe0d
minzhang190 <minzhang190@gmail.com>
2020-11-09 10:34:26 +0000

    +zodiac

---
+++ zodiac/data.js
@@ -0,0 +1,52 @@
+var title = "Shí'èr Shēngxiào";
+
+var words = {
+    "shu": {
+        "image": "assets/5fa8f807972bcd05b41ead2d.png",
+        "sound": "assets/5fa8f807972bcd05b41ead2d.mp3"
+    },
+    "niu": {
+        "image": "assets/5fa8f807972bcd05b41ead2e.png",
+        "sound": "assets/5fa8f807972bcd05b41ead2e.mp3"
+    },
+    "hu": {
+        "image": "assets/5fa8f808972bcd05b41ead2f.png",
+        "sound": "assets/5fa8f808972bcd05b41ead2f.mp3"
+    },
+    "tu": {
+        "image": "assets/5fa8f808972bcd05b41ead30.png",
+        "sound": "assets/5fa8f808972bcd05b41ead30.mp3"
+    },
+    "long": {
+        "image": "assets/5fa8f808972bcd05b41ead31.png",
+        "sound": "assets/5fa8f808972bcd05b41ead31.mp3"
+    },
+    "she": {
+        "image": "assets/5fa8f808972bcd05b41ead32.png",
+        "sound": "assets/5fa8f808972bcd05b41ead32.mp3"
+    },
+    "ma": {
+        "image": "assets/5fa8f808972bcd05b41ead33.png",
+        "sound": "assets/5fa8f808972bcd05b41ead33.mp3"
+    },
+    "yang": {
+        "image": "assets/5fa8f808972bcd05b41ead34.png",
+        "sound": "assets/5fa8f808972bcd05b41ead34.mp3"
+    },
+    "hou": {
+        "image": "assets/5fa8f808972bcd05b41ead35.png",
+        "sound": "assets/5fa8f808972bcd05b41ead35.mp3"
+    },
+    "ji": {
+        "image": "assets/5fa8f808972bcd05b41ead36.png",
+        "sound": "assets/5fa8f808972bcd05b41ead36.mp3"
+    },
+    "gou": {
+        "image": "assets/5fa8f808972bcd05b41ead37.png",
+        "sound": "assets/5fa8f808972bcd05b41ead37.mp3"
+    },
+    "zhu": {
+        "image": "assets/5fa8f809972bcd05b41ead38.png",
+        "sound": "assets/5fa8f809972bcd05b41ead38.mp3"
+    }
+};
